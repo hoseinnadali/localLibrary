93d054e4b686aaf050775823680b7002ef1e986f
hoseinnadali <hoseinnadali74@gmail.com>
2020-02-29 15:56:16 +0330

    changes

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 
 # Create your models here.
-def d(models.Model):
-  pass
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse
+from . import models
+from django.views import generic
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
+def IndexView(request):
+    return HttpResponse('etjmfg')
